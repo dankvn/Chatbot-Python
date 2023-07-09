3c8ca80ab0e8cb781edd7673393e58e4dcbe3296
dankvn <cjmayk@hotmail.com>
2023-07-09 17:56:11 -0500

    test-1

---
+++ sett.py
@@ -0,0 +1,20 @@
+token = "Meta"
+whatsapp_token = 'EAAIZA0VV4Rj0BAPuxOOIXEYcBDnb9X8VxKIpj7ADeDaOy7c4ZC4ig6ZCucDo5dOZCWBjlaRO0AvidCGufwUTTMy8BPFgk3RUnHYFCM0jtiZC96DMX0RZAvjlGPMEau5aKvYOf7imntrHengwIHWeEKJO4yWtPTS1jZAqM2u3QsYc75SbErB0u2gLYRvkyIvj5X6lbAXm90BmQZDZD'
+whatsapp_url='https://graph.facebook.com/v17.0/104668289359799/messages'
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+
+document_url = "https://www.africau.edu/images/default/sample.pdf"
